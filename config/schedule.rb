7a55bd48abb97d07261da1def790a894487c7b84
ZumeShougo <g0109725rena@gmail.com>
2022-08-04 09:33:37 +0900

    Setting cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,11 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+require File.expand_path(File.dirname(__FILE__) + '/environment')# Rails.rootを使用するために必要
+rails_env = ENV['RAILS_ENV'] || :dependent# cronを実行する環境変数
+set :enviroment, rails_env# cronを実行する環境変数をセット
+set :output, "#{Rails.root}/log/cron.log"# cronのログの吐き出し場所
+
+every :hour do#1時間ごとに（:hour）実行する先程設定したrakeタスクを記入
+  rake 'article_state:change_to_be_published'
+end
