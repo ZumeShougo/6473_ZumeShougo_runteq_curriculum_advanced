790173d0b00a9fc931075f8b5099df4e144b95dd
ZumeShougo <g0109725rena@gmail.com>
2022-08-04 09:30:03 +0900

    add publishe_wait

--- app/models/article.rb
+++ app/models/article.rb
@@ -39,7 +39,9 @@ class Article < ApplicationRecord
 
   has_one_attached :eye_catch
 
-  enum state: { draft: 0, published: 1 }
+  enum state: { draft: 0, published: 1, publish_wait: 2 }
+
+  scope :past_published, -> { where('publshed_at <= ?', Time.current) }
 
   validates :slug, slug_format: true, uniqueness: true, length: { maximum: 255 }, allow_blank: true
   validates :title, presence: true, uniqueness: true, length: { maximum: 255 }
@@ -90,4 +92,27 @@ class Article < ApplicationRecord
   def prev_article
     @prev_article ||= Article.viewable.order(published_at: :desc).find_by('published_at < ?', published_at)
   end
+
+  def publishable?
+    Time.current >= published_at
+  end
+
+  def adjust_state
+    return if draft?
+
+    self.state = if publishable?
+      :published
+    else
+      :publish_wait
+    end
+  end
+
+  def message_on_published
+    if published?
+      '記事を公開しました'
+    else
+      '記事を公開待ちにしました'
+    end
+  end
+
 end
